2739cdbf16515f8898a24d58801b2d6b64fce2e1
NEELAM AGRAWAL <nilamagrawal605@gmail.com>
2020-08-14 17:24:50 +0530

    avatar changes

--- app/src/main/java/com/example/smackchatapp/CreateUserActivity.kt
+++ app/src/main/java/com/example/smackchatapp/CreateUserActivity.kt
@@ -1,10 +1,18 @@
 package com.example.smackchatapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.ColorLong
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profiledefault"
+    var userAvatarColor = "[0.5, 0.5, 0.5, 1]"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
@@ -15,10 +23,33 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatarSignUp(view: View){
+        var randomNo = Random()
+        var color = randomNo.nextInt(2)
+        var avatarImageNo = randomNo.nextInt(28)
 
+        if(color == 0){
+            userAvatar = "light$avatarImageNo"
+        }else{
+            userAvatar = "dark$avatarImageNo"
+        }
+
+        var resourceId = resources.getIdentifier(userAvatar, "drawable",packageName)
+        imageView_Avatar.setImageResource(resourceId)
     }
 
     fun onSignUpclickedFromSignUp(view: View){
+
+        var randomColor = Random()
+        var redColor = randomColor.nextInt(255)
+        var greenColor = randomColor.nextInt(255)
+        var blueColor = randomColor.nextInt(255)
+
+        imageView_Avatar.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor))
+        var savedR = redColor.toDouble() / 255
+        var savedG = greenColor.toDouble() / 255
+        var savedB = blueColor.toDouble() / 255
+
+        userAvatarColor = "[$savedR, $savedG, $savedB, 1]"
 
     }
 }
